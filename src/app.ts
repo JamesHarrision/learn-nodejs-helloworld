e28b94aa850d5dc2755f300ffcb3df707f1ee242
Jack1e <djack1e2401@gmail.com>
2025-11-15 23:39:38 +0700

    co ban ve tham so moi truong

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express"
+import 'dotenv/config'
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send('Hello World nodemon');
@@ -13,4 +14,5 @@ app.get("/jack1e", (req, res) => {
 
 app.listen(8080, () => {
   console.log(`My app is running on port: ${PORT}`);
+  console.log(`env port: ${process.env.PORT}`);
 });
