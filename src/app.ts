f64a3a2f996a02570ff9f8e1cc00436bd1bfbbd8
Jack1e <djack1e2401@gmail.com>
2025-11-16 22:41:26 +0700

    config project for static files

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,9 @@ app.set('views', __dirname + '/views');
 //Config routes
 webRoutes(app);
 
+//Config static files: (css - images - javascripts)
+app.use(express.static('public'));
+
 app.listen(8080, () => {
   console.log(`My app is running on port: ${PORT}`);
   console.log(`${__dirname + '/views'}`);
