55b11eb5c02de4bb1ab54d5e5f7b35fc36e89270
Jack1e <djack1e2401@gmail.com>
2025-11-25 14:39:43 +0700

    giao dien view user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -44,4 +44,19 @@ const handleDeleteUser = async (id: String) => {
   }
 }
 
-export { handleCreateUser, getAllUsers, handleDeleteUser }
+const getUserById = async (id: String) => {
+  const connection = await getConnection();
+  try {
+    const sql = 'SELECT * FROM `users` WHERE `id` = ?';
+    const values = [id];
+
+    const [result, fields] = await connection.execute(sql, values);
+    return result[0];
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+
+export { handleCreateUser, getAllUsers, handleDeleteUser, getUserById}
