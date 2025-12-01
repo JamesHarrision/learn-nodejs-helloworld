9721a9ed181556a680a18f39a594c4e45c3d9c87
Jack1e <djack1e2401@gmail.com>
2025-12-01 20:49:58 +0700

    hoan thien layout admin

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,12 +1,12 @@
 import express, {Express} from 'express'
 import { getHomePage, getCreateUserPage, postCreateUser, postDeleteUser, getViewUser, postUpdateUser } from 'controllers/user.controller';
-import { getAdminUserPage, getDashboardPage } from 'controllers/admin/dashboard.controller';
+import { getAdminOrderPage, getAdminProductPage, getAdminUserPage, getDashboardPage } from 'controllers/admin/dashboard.controller';
 
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
   router.get("/", getHomePage);
-  router.get("/create-user", getCreateUserPage);
+  
   router.post("/handle-create-user", postCreateUser);
   router.post("/handle-delete-user/:id", postDeleteUser);
   router.get("/handle-view-user/:id", getViewUser);  
@@ -15,6 +15,10 @@ const webRoutes = (app: Express) => {
   //admin routes:
   router.get("/admin", getDashboardPage);
   router.get("/admin/user", getAdminUserPage);
+  router.get("/admin/order", getAdminOrderPage);
+  router.get("/admin/product", getAdminProductPage);
+  router.get("/admin/create-user", getCreateUserPage);
+
 
   app.use('/', router);
 }
