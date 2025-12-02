ee466475fce8e905995eb351d5b7b0324827f098
Jack1e <djack1e2401@gmail.com>
2025-12-02 22:52:15 +0700

    hash password

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,21 +1,26 @@
 import { prisma } from "config/client"
+import { hashPassword } from "services/user.service";
+import { ACCOUNT_TYPE } from "./constant";
 
 const initDatabase = async () => {
   const countUser = await prisma.user.count();
   const countRow = await prisma.role.count();
 
   if (countUser === 0) {
+    const defaultPassword = await hashPassword("123456");
     await prisma.user.createMany({
       data: [
         {
+          fullName: "Admin",
           username: 'abc@gmail.com',
-          password: '1234567',
-          accountType: 'system'
+          password: defaultPassword,
+          accountType: ACCOUNT_TYPE.SYSTEM
         },
         {
+          fullName: "Jack1e",
           username: 'dev@gmail.com',
-          password: '1234567',
-          accountType: 'admin'
+          password: defaultPassword,
+          accountType: ACCOUNT_TYPE.SYSTEM
         }
       ]
     });
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,18 +2,26 @@ import { prisma } from "config/client";
 import { ACCOUNT_TYPE } from "config/constant";
 import getConnection from "config/database";
 
+import bcrypt from 'bcrypt' 
+const saltRounds = 10;
+
+const hashPassword = async (plainText: string) => {
+  return await bcrypt.hash('123456', saltRounds);
+}
+
 const handleCreateUser = async (
   fullName: string, 
   email: string, 
   address: string,
   phone: string,
   avatar: string) => {
+    const defaultPassword = await hashPassword("123456");
     const user = await prisma.user.create({
       data: {
         fullName: fullName,
         username: email,
         address: address,
-        password: "123456",
+        password: defaultPassword,
         accountType: ACCOUNT_TYPE.SYSTEM,
         avatar: avatar,
         phone: phone,
@@ -73,6 +81,5 @@ const updateUserById = async (id: string,
 }
 
 
-export { handleCreateUser, getAllUsers, handleDeleteUser, getUserById, updateUserById,
-   getAllRoles
+export { handleCreateUser, getAllUsers, handleDeleteUser, getUserById, updateUserById, getAllRoles, hashPassword
 }
