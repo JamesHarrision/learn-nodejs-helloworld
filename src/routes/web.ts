d6ef1dd19bacee9f45a6a418990ab96b14bcf03a
Jack1e <djack1e2401@gmail.com>
2025-12-02 14:31:10 +0700

    upload file voi nodejs (multer)

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -2,6 +2,9 @@ import express, { Express } from 'express'
 import { getHomePage, getCreateUserPage, postCreateUser, postDeleteUser, getViewUser, postUpdateUser } from 'controllers/user.controller';
 import { getAdminOrderPage, getAdminProductPage, getAdminUserPage, getDashboardPage } from 'controllers/admin/dashboard.controller';
 
+import multer from 'multer';
+const upload = multer({ dest: 'uploads/' })
+
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
@@ -14,7 +17,10 @@ const webRoutes = (app: Express) => {
   router.get("/admin", getDashboardPage);
   router.get("/admin/user", getAdminUserPage);
   router.get("/admin/create-user", getCreateUserPage);
-  router.post("/admin/handle-create-user", postCreateUser);
+  // router.post("/admin/handle-create-user", postCreateUser);
+  router.post('/admin/handle-create-user', upload.single('avatar'), function (req, res, next) {
+    return res.send("OK");
+  });
 
   router.get("/admin/order", getAdminOrderPage);
   router.get("/admin/product", getAdminProductPage);
