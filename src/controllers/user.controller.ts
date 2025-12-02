8f6b43f7ad3dcc7094d8da8ffd9b434244feabfb
Jack1e <djack1e2401@gmail.com>
2025-12-02 14:05:28 +0700

    image preview

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,17 +11,16 @@ const getHomePage = async (req: Request, res: Response) => {
 
 const getCreateUserPage = async (req: Request, res: Response) => {
   const roles = await getAllRoles();
-  console.log(roles);
   return res.render('admin/user/create.ejs', {
     roles: roles
   });
 }
 
 const postCreateUser = async (req: Request, res: Response) => {
-  const {name, email, address} = req.body;
+  const {fullName, username, phone, role, address} = req.body;
  
   //handle create user
-  const a = await handleCreateUser(name, email, address);
+  // const a = await handleCreateUser(name, email, address);
 
   return res.redirect("/");
 }
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -6,8 +6,6 @@ const router = express.Router();
 
 const webRoutes = (app: Express) => {
   router.get("/", getHomePage);
-
-  router.post("/handle-create-user", postCreateUser);
   router.post("/handle-delete-user/:id", postDeleteUser);
   router.get("/handle-view-user/:id", getViewUser);
   router.post("/handle-update-user", postUpdateUser);
@@ -16,6 +14,7 @@ const webRoutes = (app: Express) => {
   router.get("/admin", getDashboardPage);
   router.get("/admin/user", getAdminUserPage);
   router.get("/admin/create-user", getCreateUserPage);
+  router.post("/admin/handle-create-user", postCreateUser);
 
   router.get("/admin/order", getAdminOrderPage);
   router.get("/admin/product", getAdminProductPage);
