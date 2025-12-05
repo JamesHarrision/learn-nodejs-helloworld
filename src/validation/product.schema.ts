4dd7165547a8cc5b50ab9072e5d90c6cf321d532
Jack1e <djack1e2401@gmail.com>
2025-12-05 20:35:17 +0700

    Hien thi thong bao loi

--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -1,15 +1,21 @@
-import * as z from "zod"; 
+import * as z from "zod";
 
-export const ProductSchema = z.object({ 
-  name:   z.string().trim().min(1, "Tên phải chứa ít nhất 1 ký tự"),
-  price:  z.number().min(1, "Giá phải >= 1"),
-  image:      z.string().trim().min(1),
+export const ProductSchema = z.object({
+  name: z.string().trim().min(1, "Tên phải chứa ít nhất 1 ký tự"),
+  price: z.string()
+    .transform((val) => (val === "" ? 0 : Number(val)))
+    .refine((num) => num > 0, {
+      message: "Số tiền tối thiểu là 1",
+    }),
   detailDesc: z.string().trim().min(1, "Mô tả phải có ít nhất 1 ký tự"),
-  shortDesc:  z.string().trim().min(1, "Mô tả phải có ít nhất 1 ký tự"),
-  quantity:   z.number().min(1, "Số lượng phải >= 1"),
-  sold:       z.string().trim().min(1, "Mô tả phải có ít nhất 1 ký tự"),
-  factory:    z.string().trim().min(1, "Mô tả phải có ít nhất 1 ký tự"),
-  target:     z.string().trim().min(1, "Mô tả phải có ít nhất 1 ký tự"),
+  shortDesc: z.string().trim().min(1, "Mô tả phải có ít nhất 1 ký tự"),
+  quantity: z.string()
+    .transform((val) => (val === "" ? 0 : Number(val)))
+    .refine((num) => num > 0, {
+      message: "Số lượng tối thiểu là 1",
+    }),
+  factory: z.string().trim().min(1, "NSX không được rỗng"),
+  target: z.string().trim().min(1, "TAG không được rỗng"),
 });
 
 export type TProductSchema = z.infer<typeof ProductSchema>;
